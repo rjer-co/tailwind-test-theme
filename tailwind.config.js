72156a28056b499b0a76222ef60907d8a50bf478
RR <60270210+rjer-co@users.noreply.github.com>
2024-01-29 16:53:57 -0600

    Made test change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: [],
   theme: {
+    'antique': '#EEF4EB',
+    'branch': '#5C975C',
+    'chew': '#DA82AC',
+    'dark': '#342427',
+    'fruit': '#DB5F38',
+    'glade': '#2C6F2C',
+    'jeans': '#2B6DC8',
+    'light': '#FFFEFA',
+    'lips': '#B83B3F',
+    'sepia': '#453130',
+    'wash': '#79A5E0',
     extend: {},
   },
   plugins: [],
